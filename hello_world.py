425c4ff52666438388699082d8f6073bad3adfbc
DONGHUI ZHOU <69742741+zhong1972@users.noreply.github.com>
2024-09-15 00:27:29 -0400

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+import datetime
+def main():
+    print("Hello, World!")
+    
+    # Get the current time and print it
+    current_time=datetime.datetime.now()
+    print("Current time:", current_time)
+if __name__ == "__main__":
+    main()
